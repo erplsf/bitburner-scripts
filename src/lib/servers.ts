f81ebfa13a485031bd5efcaec87eaf99d207b025
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-01-08 20:22:43 +0100

    add rooter and friends

--- src/lib/servers.ts
+++ src/lib/servers.ts
@@ -4,11 +4,33 @@ export async function main(ns: NS): Promise<void> {
   ns.tprint(serverList(ns));
 }
 
-export function serverList(ns: NS): string[] {
+interface Server {
+  name: string
+  freeRAM: number
+  root: boolean
+  requiredLevel: number
+  requiredPorts: number
+}
+
+export function serverList(ns: NS): Server[] {
   let allServers: Set<string> = new Set(['home']);
   allServers.forEach((server) => {
     const servers = ns.scan(server);
     servers.forEach((foundServer) => allServers.add(foundServer));
   });
-  return Array.from(allServers.values())
+  return Array.from(allServers.values()).map(host => buildServer(ns, host))
+}
+
+function buildServer(ns: NS, host: string): Server {
+  return {
+    name: host,
+    freeRAM: getServerFreeRAM(ns, host),
+    root: ns.hasRootAccess(host),
+    requiredLevel: ns.getServerRequiredHackingLevel(host),
+    requiredPorts: ns.getServerNumPortsRequired(host),
+  }
+}
+
+export function getServerFreeRAM(ns: NS, host: string): number {
+  return ns.getServerMaxRam(host) - ns.getServerUsedRam(host)
 }
---
+++ src/rooter.ts
@@ -0,0 +1,42 @@
+import { NS } from "@ns";
+import { serverList } from "lib/servers";
+
+type Opener = 'brutessh'
+
+const openers: Opener[] = [
+    'brutessh'
+]
+
+export async function main(ns: NS): Promise<void> {
+  let currentHackingLevel = ns.getHackingLevel()
+  for (;;) {
+    const nonRootedServers = serverList(ns).filter(server => !server.root)
+    if (nonRootedServers.length == 0) break
+    const availableOpeners = getAvailableOpeners(ns)
+    const rootableServers = nonRootedServers.filter(
+        server => server.requiredLevel <= currentHackingLevel &&
+                  server.requiredPorts <= availableOpeners.length
+    )
+    for (const server of rootableServers) {
+        applyOpeners(ns, server.name, availableOpeners)
+        ns.nuke(server.name)
+    }
+    while (currentHackingLevel == ns.getHackingLevel()) await ns.sleep(1000)
+    currentHackingLevel = ns.getHackingLevel()
+  }
+}
+
+function getAvailableOpeners(ns: NS): Opener[] {
+    return openers.filter(opener => ns.fileExists(`${opener}.exe`, 'home'))
+}
+
+function applyOpeners(ns: NS, host: string, openers: Opener[]) {
+    for (const opener of openers) {
+        switch(opener) {
+                case 'brutessh': {
+                    ns.brutessh(host)
+                    break
+                }
+        }
+    }
+}
