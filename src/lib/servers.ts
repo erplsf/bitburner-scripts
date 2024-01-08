65a984c205546b774e41e78c8a08fad6e76987e9
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-01-08 16:32:16 +0100

    adjust servers

--- src/lib/servers.ts
+++ src/lib/servers.ts
@@ -1,10 +1,14 @@
 import { NS } from "@ns";
 
 export async function main(ns: NS): Promise<void> {
+  ns.tprint(serverList(ns));
+}
+
+export function serverList(ns: NS): string[] {
   let allServers: Set<string> = new Set(['home']);
   allServers.forEach((server) => {
     const servers = ns.scan(server);
     servers.forEach((foundServer) => allServers.add(foundServer));
   });
-  ns.tprint(allServers);
+  return Array.from(allServers.values())
 }
